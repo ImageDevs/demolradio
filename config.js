ad8299dac924088be1ad4ac36c4935f1e9b1c364
Benito <benito.herremans@gmail.com>
2026-06-27 19:36:58 +0200

    Add Danish language station at 33.3

--- config.js
+++ config.js
@@ -3,6 +3,7 @@
 window.RADIO_CONFIG = {
   STATIONS: [
     { TARGET: 13.3, TOLERANCE: 0.2, AUDIO_SRC: "kate.mp3",      LOOP: true  },
-    { TARGET: 19.4, TOLERANCE: 0.2, AUDIO_SRC: "broadcast.mp3", LOOP: false }
+    { TARGET: 19.4, TOLERANCE: 0.2, AUDIO_SRC: "broadcast.mp3", LOOP: false },
+    { TARGET: 33.3, TOLERANCE: 0.2, AUDIO_SRC: "danish.mp3",    LOOP: false }
   ]
 };
