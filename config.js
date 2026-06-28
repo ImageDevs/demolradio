f282859a78027a5b8d548810e634497aeb2e5974
Benito <benito.herremans@gmail.com>
2026-06-28 10:12:20 +0200

    Set station frequencies to permutations of 19.4

--- config.js
+++ config.js
@@ -2,11 +2,11 @@
 
 window.RADIO_CONFIG = {
   STATIONS: [
-    { TARGET: 13.3, TOLERANCE: 0.2, AUDIO_SRC: "kate.mp3",      LOOP: true,  START: 10 },
+    { TARGET: 14.9, TOLERANCE: 0.2, AUDIO_SRC: "kate.mp3",      LOOP: true,  START: 10 },
     { TARGET: 19.4, TOLERANCE: 0.2, AUDIO_SRC: "broadcast.mp3", LOOP: false },
-    { TARGET: 33.3, TOLERANCE: 0.2, AUDIO_SRC: "danish.mp3",    LOOP: false },
-    { TARGET: 48.6, TOLERANCE: 0.2, AUDIO_SRC: "station5.mp3",  LOOP: false },
-    { TARGET: 71.8, TOLERANCE: 0.2, AUDIO_SRC: "station4.mp3",  LOOP: false },
-    { TARGET: 87.3, TOLERANCE: 0.2, AUDIO_SRC: "station6.mp3",  LOOP: false }
+    { TARGET: 41.9, TOLERANCE: 0.2, AUDIO_SRC: "danish.mp3",    LOOP: false },
+    { TARGET: 49.1, TOLERANCE: 0.2, AUDIO_SRC: "station5.mp3",  LOOP: false },
+    { TARGET: 91.4, TOLERANCE: 0.2, AUDIO_SRC: "station4.mp3",  LOOP: false },
+    { TARGET: 94.1, TOLERANCE: 0.2, AUDIO_SRC: "station6.mp3",  LOOP: false }
   ]
 };
